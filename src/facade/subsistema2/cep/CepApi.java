f9f5fd7360618f3965a9c819af13c95223295b2a
GA9BR1 <gustavoalberttodev@gmail.com>
2023-01-26 16:18:20 -0300

    first commit

---
+++ src/facade/subsistema2/cep/CepApi.java
@@ -0,0 +1,23 @@
+package facade.subsistema2.cep;
+
+public class CepApi {
+
+    private static class InstanceHolder {
+        public static CepApi instancia = new CepApi();
+    }
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstancia() {
+        return InstanceHolder.instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Araraquara";
+    }
+    public String recuperarEstado(String cep) {
+        return "SP";
+    }
+}
---
+++ src/gof/singleton/Test.java
@@ -0,0 +1,26 @@
+package gof.singleton;
+
+import gof.singleton.SingletonEager;
+import gof.singleton.SingletonLazy;
+import gof.singleton.SingletonLazyHolder;
+
+public class Test {
+    public static void main(String[] args) {
+
+        // Testes do Design pattern Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+    }
+}
